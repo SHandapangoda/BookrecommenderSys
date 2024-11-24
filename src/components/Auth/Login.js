613f10bc499d28cb0d14900482e8136dbe398425
Sithum Handapangoda <hsithum98@yahoo.com>
2024-11-24 23:27:09 +0530

    Initial project version

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -13,7 +13,7 @@ const Login = () => {
       const response = await fetch("http://localhost:5000/api/auth/login", {
         method: "POST",
         headers: {
-          "Content-Type": "application/json",
+          "Content-Type": "application/json"
         },
         body: JSON.stringify({ email, password }),
       });
