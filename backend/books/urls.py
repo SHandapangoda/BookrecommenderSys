f050e2d558aaa7fd427a87ba17d3c1a54ae6b393
Sithum Handapangoda <hsithum98@yahoo.com>
2024-12-19 21:21:24 +0530

    version1

--- backend/books/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import BookListCreateView, ReviewListCreateView
-
-urlpatterns = [
-    path('books/', BookListCreateView.as_view(), name='book-list'),
-    path('reviews/', ReviewListCreateView.as_view(), name='review-list'),
-]
